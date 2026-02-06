2f60ad6160f00254af0491616058243af633e0f2
ramya033 <ramyavk42@gmail.com>
2026-02-06 12:45:56 +0530

    examples of variablesAnddatatypes

---
+++ Refrigerator.java
@@ -0,0 +1,35 @@
+class Refrigerator{
+	    static String brand="LG";
+		static int capacity=260;
+		static int star=3;
+		static String type="Double Door";
+		static boolean inverter=true;
+		static String color="Silver";
+		static int shelves=4;
+		static String cooling="Frost Free";
+		static int warranty=10;
+		static float price=28999F;
+	    /*public static void main(String[] args){
+		String brand="LG";
+		int capacity=260;
+		int star=3;
+		String type="Double Door";
+		boolean inverter=true;
+		String color="Silver";
+		int shelves=4;
+		String cooling="Frost Free";
+		int warranty=10;
+		float price=28999F;*/
+
+		System.out.println("Brand: "+brand);
+		System.out.println("Capacity: "+capacity);
+		System.out.println("Star: "+star);
+		System.out.println("Type: "+type);
+		System.out.println("Inverter: "+inverter);
+		System.out.println("Color: "+color);
+		System.out.println("Shelves: "+shelves);
+		System.out.println("Cooling: "+cooling);
+		System.out.println("Warranty: "+warranty);
+		System.out.println("Price: "+price);
+	}
+}
---
+++ Scanner.java
@@ -0,0 +1,35 @@
+class Scanner{
+		static String brand="Canon";
+		static String type="Flatbed";
+		static int resolution=1200;
+		static String color="Black";
+		static boolean usb=true;
+		static String format="PDF";
+		static boolean portable=false;
+		static int warranty=1;
+		static String speed="Fast";
+		static float price=8999F;
+		/*public static void main(String[] args){
+		String brand="Canon";
+		String type="Flatbed";
+		int resolution=1200;
+		String color="Black";
+		boolean usb=true;
+		String format="PDF";
+		boolean portable=false;
+		int warranty=1;
+		String speed="Fast";
+		float price=8999F;*/
+
+		System.out.println("Brand: "+brand);
+		System.out.println("Type: "+type);
+		System.out.println("Resolution: "+resolution);
+		System.out.println("Color: "+color);
+		System.out.println("USB: "+usb);
+		System.out.println("Format: "+format);
+		System.out.println("Portable: "+portable);
+		System.out.println("Warranty: "+warranty);
+		System.out.println("Speed: "+speed);
+		System.out.println("Price: "+price);
+	}
+}
